2c2cda7a65595f1a2a8aa06765d269ae13087c51
Tharamac <tharathorn.wang@gmail.com>
2023-10-30 01:48:18 +0700

    [form-validation] error-1

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,17 +1,17 @@
 import { CardPattern, WishEntry } from "@/models/wish_entry";
 import { Button, CircularProgress, Divider} from '@mui/material'
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler, UseFormSetError } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
-
+export type Inputs = {
+    senderName: string,
+    wishMessage: string,
+    cardPattern: string,
+    isConsentPublish: boolean,
+};
 
 export default function Form() {
-    type Inputs = {
-        senderName: string,
-        wishMessage: string,
-        cardPattern: string,
-        isConsentPublish: boolean,
-    };
-
+ 
 
     const fetchData = async () => {
         const response = await fetch('/api/storeJSONData')
@@ -36,31 +36,68 @@ export default function Form() {
           console.log(data);
     }
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>( {defaultValues: { isConsentPublish: false }} );
-    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
-    const onInvalid = (errors: any) => console.error(errors)
+    const { register, handleSubmit, watch, setError, formState: { errors } } = useForm<Inputs>( {defaultValues: { isConsentPublish: false }}, );
+    const onSubmit: SubmitHandler<Inputs> = data => {
+        console.log(!data.cardPattern)
+        // const inputs = [
+        //     {
+        //       type: "manual",
+        //       name: "cardPattern",
+        //       message: "Double Check This",
+        //     },
+        //     {
+        //       type: "manual",
+        //       name: "isConsentPublish",
+        //       message: "Triple Check This",
+        //     },
+        //   ]
 
+        //   inputs.forEach(({ type, name,  message }) => {
+        //     setError(name, { type, message })
+        //   })
+        if(!data.cardPattern)
+        {  
+            setError("cardPattern", {
+                type:  "manual",
+                message: "กรุณายินยอม"
+            });
+        }
+        if(!data.isConsentPublish){
+            setError("isConsentPublish", {
+                type: "manual",
+                message: "กรุณายินยอม"
+            });
+        }
+        const entry = WishEntry.fromInputs(data);
+        console.log(entry);
+    }
+    const onInvalid = (errors: any) => console.error(errors)
+ 
     //min-[341px]:py-4
-    //onSubmit={handleSubmit(onSubmit)}
+    //onSubmit={handleSubmit(onSubmit)}w
     return (
         <form onSubmit={handleSubmit(onSubmit, onInvalid)} className="bg-[#FDA172] w-auto md:w-[640px] rounded-2xl px-4 mx-4 sm:mx-16 md:mx-auto pt-4 pb-8 items-center h-full justify-center">
-            <button onClick={fetchData}>Fetch</button>
-            <button onClick={saveData}>Save</button>
+            {/* <button onClick={fetchData}>Fetch</button>
+            <button onClick={saveData}>Save</button> */}
             <div className="space-y-12">
                 <div className="border-b border-gray-900/10 pb-12">
                     <h2 className="text-xl font-semibold leading-7 justify-center">อวยพรวันเกิด</h2>
                     {/* <p className="mt-1 text-sm leading-6 text-white">This information will be displayed publicly so be careful what you share.</p> */}
-
                     <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                         <div className="sm:col-span-4">
                             <label htmlFor="sender" className="block text-sm font-medium leading-6 text-gray-900">จาก</label>
                             <div className="mt-2">
                                 <div className="flex rounded-md ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-gray-200 sm:max-w-md">
                                     <input type="text" id="sender"
-                                        {...register("senderName", { required: true, maxLength: 40 })}
+                                        {...register("senderName", {required: "\"แกไม่มีสิทธิ์ ไม่บอกชื่อ\" - คัมภีร์ไม่ได้กล่าว", maxLength: 40 })}
                                         className="block flex-1 border-0 rounded-md bg-white py-1.5 px-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                        placeholder="ชื่อที่อยากให้ปรากฏในการ์ดอวยพร"></input>
+                                        placeholder="ชื่อที่อยากให้ปรากฏในการ์ดอวยพร"></input>           
                                 </div>
+                                <ErrorMessage
+                                    errors={errors}
+                                    name="senderName"
+                                    render={({ message }) => <p className="text-[#b90e0a] mt-2">{message}</p>}
+                                />
                             </div>
                         </div>
 
@@ -68,16 +105,20 @@ export default function Form() {
                             <label htmlFor="wishing" className="block text-sm font-medium leading-6 text-gray-900">คำอวยพร</label>
                             <div className="mt-2">
                                 <textarea id="wishing_message" 
-                                    {...register("wishMessage", { required: true, })}
+                                    {...register("wishMessage", { required: "\"นี่กะจะไม่เขียนอะไรเลยหรอห้ะ\" - คัมภีร์ไม่ได้กล่าว", })}
                                     className="block w-full rounded-md border-0 py-1.5 px-2 h-[200px] text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
                                     placeholder="เช่น ขอให้ไม่ปวดหลัง"
                                 ></textarea>
                             </div>
+                            <ErrorMessage
+                                    errors={errors}
+                                    name="wishMessage"
+                                    render={({ message }) => <p className="text-[#b90e0a] mt-2">{message}</p>}
+                                />
                             {/* <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences wishing yourself.</p> */}
                         </div>
 
                         <div className="col-span-full">
-
                             <fieldset >
                                 <legend className="text-sm font-semibold leading-6 text-gray-900 flex">เลือกลายการ์ด</legend>
 
@@ -92,7 +133,7 @@ export default function Form() {
                                             className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                                         />
                                         <label htmlFor="push-everything" className="block text-sm font-medium leading-6 text-gray-900">
-                                            แพะ
+                                            แพะส้ม
                                         </label>
                                     </div>
                                     <div className="flex items-center gap-x-3">
@@ -103,9 +144,9 @@ export default function Form() {
                                             value="withSage"
                                             className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                                         />
-                                        {/* <label htmlFor="push-email" className="block text-sm font-medium leading-6 text-gray-900">
-                                            หน้ากาก
-                                        </label> */}
+                                        <label htmlFor="push-email" className="block text-sm font-medium leading-6 text-gray-900">
+                                            น้ามา
+                                        </label>
                                     </div>
                                     <div className="flex items-center gap-x-3">
                                         <input
@@ -116,11 +157,17 @@ export default function Form() {
                                             className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                                         />
                                         <label htmlFor="push-nothing" className="block text-sm font-medium leading-6 text-gray-900">
-                                            เสื้อกล้าม
+                                            บิกินี่
                                         </label>
                                     </div>
                                 </div>
                             </fieldset>
+                            {/* {errors. && <p>{errors.username.message}</p>} */}
+                            <ErrorMessage
+                                    errors={errors}
+                                    name="cardPattern"
+                                    render={({ message }) => <p className="text-[#b90e0a] mt-2">{message}</p>}
+                                />
 
                         </div>
 
@@ -157,10 +204,16 @@ export default function Form() {
                         <label htmlFor="consent"  className="font-medium text-gray-900">ยินยอมให้เผยแพร่คำอวยพรนี้สู่สาธารณะ</label>
                         {/* <p className="text-gray-500">Get notified when someones posts a comment on a posting.</p> */}
                     </div>
+                    <ErrorMessage
+                                    errors={errors}
+                                    name="isConsentPublish"
+                                    render={({ message }) => <p className="text-[#b90e0a] mt-2">{message}</p>}
+                                />
                 </div>
                 <div className="flex items-center gap-x-6">
                     <button type="reset" className="text-sm font-semibold leading-6 text-gray-900">เคลียร์ฟอร์ม</button>
-                    <button type="submit" className="rounded-md bg-orange-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">ส่งคำอวยพร</button>
+                    <button type="submit" 
+                    className="rounded-md bg-orange-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">ส่งคำอวยพร</button>
                 </div>
                
             </div>

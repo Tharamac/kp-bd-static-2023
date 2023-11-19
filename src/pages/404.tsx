6c546019538f8b1512165663c0f055a505d5d97b
Tharathorn <tharathorn.w@fourgle.com>
2023-11-20 00:04:13 +0700

    error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,10 +9,13 @@ import { faDiscord, faFacebook, faTiktok } from '@fortawesome/free-brands-svg-ic
 import { Logo } from "@/svg/Logo";
 
 export default function Page404() {
-    return <div className="flex flex-col relative w-full h-screen justify-center items-center overflow-hidden gap-4 bg-[#ed9453]">
-          <div className="text-black text-8xl z-10 w-full text-center ">404 </div>
-        <div className="text-black text-4xl z-10 w-full text-center p-16">หาหน้าเว็บไม่เจอสินะ ไม่เป็นไร! เรามาลองใหม่อีกครั้ง แต่ดู <b className='text-[#ae4304] underline'>คัมภีร์บิกินี่</b> ก่อนเพื่อความเป็นสิริมงคล //แปะรูปเคพีบิกินี่ </div>
-            <div className="flex flex-row gap-6 justify-left text-white items-center">
+    return <div className="flex flex-col relative w-full h-screen justify-center items-center overflow-hidden gap-4 bg-[#ed9453] p-32">
+        <div className="flex flex-row">        <img src="img\404\404.png" className="w-[400px]  rounded-full"/>
+            <div className="flex flex-col justify-left items-left">        
+                <div className="text-black text-8xl font-bold z-10 w-full">404 </div>
+                     <div className="text-black text-4xl z-10 w-full pt-4">หาหน้าเว็บไม่เจอสินะ ไม่เป็นไร! เรามาลองใหม่อีกครั้ง แต่ดู <b className='text-[#ae4304] underline'>คัมภีร์บิกินี่</b> ก่อนเพื่อความเป็นสิริมงคล //แปะรูปเคพีบิกินี่ </div>
+                     
+                               <div className="flex flex-row gap-6 justify-left text-white items-center pt-8">
                 <Link className="flex flex-row items-center gap-4" href={"/"}>
                     <HomeIcon className="text-2xl"/>
                     <span className="text-2xl text-black"><b>คุณอยากมาหน้านี้สินะ อวยพรคัมภีร์น่ะ</b></span>
@@ -26,6 +29,12 @@ export default function Page404() {
                         <span className="min-[431px]:text-2xl text-2xl text-black"> <b>ดิสคอร์ดแฟนคลับของคัมภีร์</b></span>
                     </Link>
             </div>
+                     
+                     </div>
+
+        </div>
+
+  
             
     
     </div>
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -9,24 +9,35 @@ import { faDiscord, faFacebook, faTiktok } from '@fortawesome/free-brands-svg-ic
 import { Logo } from "@/svg/Logo";
 
 export default function Page500() {
-    return <div className="flex flex-col relative w-full h-screen justify-center items-center overflow-hidden gap-4 bg-[#ed9453]">
-    <div className="text-black text-8xl z-10 w-full text-center ">500 </div>
+
+return <div className="flex flex-col relative w-full h-screen justify-center items-center overflow-hidden gap-4 bg-[#ed9453] p-32">
+        <div className="flex flex-row">        
+        
+        <img src="img\404\404.png" className="w-[400px]  rounded-full"/>
+            <div className="flex flex-col justify-left items-left">        
+            <div className="text-black text-8xl z-10 w-full text-center ">500 </div>
 <div className="text-black text-4xl z-10 w-full text-center p-16">โหลดหน้าเว็บไม่ขึ้นสินะ ไม่เป็นไร! เรามาลองใหม่อีกครั้ง แต่ดู <b className='text-[#ae4304] underline'>คัมภีร์บิกินี่</b> ก่อนเพื่อความเป็นสิริมงคล //แปะรูปเคพีบิกินี่ </div>
-      <div className="flex flex-row gap-6 justify-left text-white items-center">
-          <Link className="flex flex-row items-center gap-4" href={"/"}>
-              <HomeIcon className="text-2xl"/>
-              <span className="text-2xl text-black"><b>คุณอยากมาหน้านี้สินะ อวยพรคัมภีร์น่ะ</b></span>
-          </Link> 
+                               <div className="flex flex-row gap-6 justify-left text-white items-center pt-8">
+                <Link className="flex flex-row items-center gap-4" href={"/"}>
+                    <HomeIcon className="text-2xl"/>
+                    <span className="text-2xl text-black"><b>คุณอยากมาหน้านี้สินะ อวยพรคัมภีร์น่ะ</b></span>
+                </Link> 
+        
+                    <Divider className="bg-black" orientation="vertical" flexItem  />
+   
+
+                    <Link className="flex min-[431px]:flex-row flex-col items-center gap-4" href={"https://discord.gg/p2fY7N4Mcb"} target="_blank">
+                        <FontAwesomeIcon className="text-2xl aspect-square" icon={faDiscord} />
+                        <span className="min-[431px]:text-2xl text-2xl text-black"> <b>ดิสคอร์ดแฟนคลับของคัมภีร์</b></span>
+                    </Link>
+            </div>
+                     
+                     </div>
+
+        </div>
+
   
-              <Divider className="bg-black" orientation="vertical" flexItem  />
-
-
-              <Link className="flex min-[431px]:flex-row flex-col items-center gap-4" href={"https://discord.gg/p2fY7N4Mcb"} target="_blank">
-                  <FontAwesomeIcon className="text-2xl aspect-square" icon={faDiscord} />
-                  <span className="min-[431px]:text-2xl text-2xl text-black"> <b>ดิสคอร์ดแฟนคลับของคัมภีร์</b></span>
-              </Link>
-      </div>
-      
-
-</div>
+            
+    
+    </div>
 }
